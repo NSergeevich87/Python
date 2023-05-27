1c25b9787c3f912be39a9215131acfdd9e3f42e4
NSergeevich87 <niknikitaser@gmail.com>
2023-05-27 08:30:38 +0200

    practicum

--- Practicum_Python/program.py
+++ Practicum_Python/program.py
@@ -1,8 +1,22 @@
-first_friend_speed = 1
-second_friend_speed = 2
-dog_speed = 5
-distance = 1000
-distance_limit = 10
-dog_counter = 0
+# Sample Input 1:
+# 150
+# Sample Output 1:
+# 150 мин - это 2 час 30 минут.
 
-t = s / v
+
+x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+if x2 > y1 or x1 > y2: print('пустое множество')
+elif y1 == x2: print(y1)
+elif y2 == x1: print(y2)
+elif x2 >= x1 and y2 >= y1:
+    print(x2, y1)
+elif x1 >= x2 and y1 >= y2:
+    print(x1, y2)
+    # ОТРИЦАТЕЛЬНЫЕ ЗНАЧЕНИЯ:
+elif x1 < 0 or x2 < 0 or y1 < 0 or y2 < 0:
+    if x2 >= x1 and y2 <= y1:
+        print(x2, y2)
+    if x1 >= x2 and y1 <= y2:
+        print(x1, y1)
+
+
