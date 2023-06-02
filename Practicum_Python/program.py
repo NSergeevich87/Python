b8a67ee78fd132c1a1f3a22e334bf70b7dddfaaa
NSergeevich87 <niknikitaser@gmail.com>
2023-06-02 16:03:40 +0200

    practice

--- Practicum_Python/program.py
+++ Practicum_Python/program.py
@@ -1,9 +1,13 @@
-i = 0
-nums = list()
-while i < 5:
-    nums.append(int(input()))
-    i += 1
-sum_ = 0
-for i in range(len(nums)):
-    sum_ += abs(nums[i])
-print(sum_)
+from math import sqrt
+
+a, b, c = float(input()), float(input()), float(input())
+
+d = pow(b, 2) - 4*a*c
+
+if d > 0:    
+    print(min(((-b - sqrt(d)) / (2*a)), ((-b + sqrt(d)) / (2*a))))
+    print(max(((-b - sqrt(d)) / (2*a)), ((-b + sqrt(d)) / (2*a))))
+elif d == 0:
+    print(-b / (2*a))
+elif d < 0:
+    print('Нет корней')
