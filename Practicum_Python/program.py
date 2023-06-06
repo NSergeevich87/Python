76c39f98c1f8a0d5f604945649d174cba674909a
NSergeevich87 <niknikitaser@gmail.com>
2023-06-06 20:55:48 +0200

    mod

--- Practicum_Python/program.py
+++ Practicum_Python/program.py
@@ -1,7 +1,16 @@
-m, n = int(input()), int(input())
-if m < n:
-    for i in range(m, n + 1):
-        print(i)
-elif m > n:
-    for i in range(m, n - 1, -1):
-        print(i)
+n = int(input())
+old_list = list()
+for i in range(n):
+    old_list.append(int(input()))
+
+new_list = sorted(old_list)
+print(new_list)
+print(max(new_list))
+
+flag = False
+
+for i in new_list:
+    while flag == False:
+        if new_list[-i] < max(new_list):
+            print(new_list[-i])
+            flag = True
