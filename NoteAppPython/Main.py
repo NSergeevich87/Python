72e491f82e92afe2636e699f6e3e228732cc8854
NSergeevich87 <niknikitaser@gmail.com>
2023-07-31 16:53:45 +0200

    allTaskDone

--- NoteAppPython/Main.py
+++ NoteAppPython/Main.py
@@ -1,58 +1,119 @@
-SaveBook = r'NoteAppPython\SaveBook.txt'
+import os
+import time
 
-def write_contacts(SaveBook):
-    with open(SaveBook, 'a', encoding='utf-8') as file:
-        file.write('\n' + str.lower(input(f'Введите через пробел: ФИО и номер телефона -> ')))
-    return file
+def create_note():
+    filename = input("Введите название заметки: ") + ".txt"
+    with open(filename, "w") as file:
+        content = input("Напишите что-нибудь в заметку: ")
+        file.write(content)
 
-def read_contacts(SaveBook):
-    with open(SaveBook, 'r', encoding='utf-8') as file:
-        for line in file:
-            print(line)
+def show_notes():
+    files = os.listdir()
+    txt_files = [file for file in files if file.endswith(".txt")]
 
-def find_contacts(SaveBook):
-    find = str.lower(input('Введите данные для поиска: '))
-    with open(SaveBook, 'r', encoding='utf-8') as file:
-        for line in file:
-            if find in line:
-               print(line)
+    print("Список ваших заметок:")
+    for file in txt_files:
+        print(f"- {file}")
 
-def edit_contacts(SaveBook):
-    words = str.lower(input('Введите, что будем менять -> '))
-    with open(SaveBook, 'r', encoding='utf-8') as file:
-        old_data = file.read()
-        new_data = old_data.replace(words, str.lower(input('Введите на что заменить -> ')))
-    with open(SaveBook, 'w', encoding='utf-8') as file:
-        file.write(new_data)
+    filename = input("Введите имя заметки для просмотра: ") + ".txt"
+    if filename in txt_files:
+        with open(filename, "r") as file:
+            content = file.read()
+            print(content)
+    else:
+        print("Заметка не найдена!")
+
+def del_note():
+    filename = input("Введите имя файла, который вы хотите удалить: ") + ".txt"
+    if os.path.exists(filename):
+        os.remove(filename)
+        print(f"Файл {filename} удален.")
+    else:
+        print("Файл не найден.")
         
+def edit_note():
+    filename = input("Введите имя заметки, которую вы хотите редактировать: ") + ".txt"
+    with open(filename, "r") as file:
+        content = file.read()
+        print(f'Текущее содержимое заметки "{filename}":')
+        print(content)
+
+    edit_mode = input("Хотите перезаписать всю заметку (введите 'всю')\n отредактировать заметку частично (введите 'частично')\n или продолжить запись (введите 'продолжить')? ")
+    if edit_mode == "всю":
+        new_content = input("Введите новый текст: ")
+        with open(filename, "w") as file:
+            file.write(new_content)
+            print(f'Заметка "{filename}" обновлена.')
+    elif edit_mode == "частично":
+        lines = content.split("\n")
+        for i, line in enumerate(lines):
+            print(f"{i + 1}: {line}")
+
+        line_num = int(input("Введите номер строки, которую вы хотите изменить: "))
+        new_line = input("Введите новый текст для этой строки: ")
+        lines[line_num - 1] = new_line
+
+        with open(filename, "w") as file:
+            file.write("\n".join(lines))
+            print(f'Заметка "{filename}" обновлена.')
+    elif edit_mode == "продолжить":
+        new_content = input("Введите текст, который вы хотите добавить: ")
+        with open(filename, "a") as file:
+            file.write("\n" + new_content)
+            print(f'Заметка "{filename}" обновлена.')
+    else:
+        print("Неверный режим редактирования.")
+
+def find_note_for_date():
+    date_str = input("Введите дату в формате ГГГГ-ММ-ДД: ")
+    date = time.strptime(date_str, "%Y-%m-%d")
+
+    files = os.listdir()
+    txt_files = [file for file in files if file.endswith(".txt")]
+
+    print(f"Заметки созданные {date_str}:")
+    for file in txt_files:
+        file_date = time.localtime(os.path.getctime(file))
+        if file_date.tm_year == date.tm_year and file_date.tm_mon == date.tm_mon and file_date.tm_mday == date.tm_mday:
+            print(f"- {file}")
 
 flag = True
 while flag:
+    print('\nДобро пожаловать в приложение "Заметки"!\n')
     ask = int(input(
-        '1 - для ввода,' +  
-        '2 - для вывода,' + 
-        '3 - для поиска,' + 
-        '4 - для редактирования или удаления \n'))
+        '1 - создать новую заметку\n' +  
+        '2 - вывести список заметок\n' + 
+        '3 - для поиска по дате\n' + 
+        '4 - для редактирования\n' +
+        '5 - для удаления\n' + 
+        '6 - для выхода\n'))
     if ask == 1:
-        write_contacts(SaveBook)
+        create_note()
         answ = str.lower(input('Продолжаем? да/нет -> '))
         if answ == 'нет': flag = False
         else: continue
     elif ask == 2:
-        read_contacts(SaveBook)
+        show_notes()
         answ = str.lower(input('Продолжаем? да/нет -> '))
         if answ == 'нет': flag = False
         else: continue
     elif ask == 3:
-        find_contacts(SaveBook)
+        find_note_for_date()
         answ = str.lower(input('Продолжаем? да/нет -> '))
         if answ == 'нет': flag = False
         else: continue
     elif ask == 4:
-        edit_contacts(SaveBook)
+        edit_note()
         answ = str.lower(input('Продолжаем? да/нет -> '))
         if answ == 'нет': flag = False
         else: continue
+    elif ask == 5:
+        del_note()
+        answ = str.lower(input('Продолжаем? да/нет -> '))
+        if answ == 'нет': flag = False
+        else: continue
+    elif ask == 6:
+        flag = False
     else:
         print('Нет такой функции!')
         
